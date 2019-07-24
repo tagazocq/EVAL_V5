a352cc9d0a3614a23f496e8c7eaed588363120b0
tagazocq <wbrahy@gmail.com>
2019-07-24 22:22:21 +0200

    Revert "Initial commit"
    This reverts commit 27edc73b22ee5ae6ed80f6e043e705291d472a97.

--- include/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-const MYHOST = '192.168.128.13';
-const MYUSER = 'in18b1075'; //votre nom d'utilisateur de base de donnée
-const MYPASS = '7753';     //votre mot de passe associé
-const MYDB   = 'in18b1075'; //le nom de votre base de donnée qui, à HELMo,
-// est identique à votre nom d'utilisateur
-?>
